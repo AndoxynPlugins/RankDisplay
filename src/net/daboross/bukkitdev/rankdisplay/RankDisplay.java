781c2a4a661aa373a6dd983c4d5cf4d9600408bd
Dabo Ross <dabodialga@gmail.com>
2013-05-01 15:18:08 -0700

    Initial Commit

---
+++ src/net/daboross/bukkitdev/rankdisplay/RankDisplay.java
@@ -0,0 +1,35 @@
+
+package net.daboross.bukkitdev.rankdisplay;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author daboross
+ */
+public class RankDisplay extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent pje) {
+    }
+
+}
